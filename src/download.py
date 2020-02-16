e2ab913e35597de71d5d9f797677293ad82e5a2f
gomuse <gomuse@gomuseui-MacBookPro.local>
2020-02-16 21:55:03 +0900

    다운로드 모듈 작성

---
+++ src/download.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+
+def get_tube(url):
+    tube = YouTube(url)
+    return tube
+
+def get_streams(tube, **kwargs):
+    streams = tube.streams.filter(**kwargs)
+    return streams
